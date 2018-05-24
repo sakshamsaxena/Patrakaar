ccdea0e2ec0f70208ba3af48a8b18e81ba75bca9
Saksham Saxena <saksham_saxena@outlook.com>
2018-05-24 20:16:01 +0530

    Exports CSV now

--- Patrakaar.js
+++ Patrakaar.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 // Modules to be used
+var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 
@@ -40,7 +41,7 @@ var L = [];
 var C = concurrency;
 var U = url;
 var trueCount = 0, next = C;
-var map = {};
+var map = {}, csv = "";
 var $;
 var successfulR = 0, failedR = 0;
 
@@ -54,21 +55,22 @@ request(U, function(error, response, body) {
 		// Got the HTML
 		$ = cheerio.load(body);
 		// Extract Unique Links only
+		csv+="1,\n";
 		$("body a").each(function() {
 			var key = $(this).attr("href");
+			// TODO : Check if this is worthy of counting or not
 			if(map[key] === undefined) {
 				map[key] = 1;
 			}
 		});
-		// Set our list up
+		// Set our initial list up
 		L = Object.keys(map);
 		trueCount = L.length;
-		console.log(trueCount);
-		// Clear the map, save memory!
-		map = {};
-		console.log('Got it!');
+		console.log(`Got ${trueCount} links!`);
+		// Update CSV
+		csv+=L.toString();
+		csv+="\n2,\n";
 		// Begin Crawling
-		// TODO
 		process.nextTick(function() {
 			for (let i = 0; i < C; i++) {
 				let link = L[i];
@@ -87,10 +89,25 @@ function launchRequest(u, ind) {
 	request(u, function(err, resp, body) {
 		if(!err && resp.statusCode == 200) {
 			// TODO : Do what you want with this response
+			// Got the HTML
+			$ = cheerio.load(body);
+			// Extract Unique Links only
+			let tempMap = {};
+			$("body a").each(function() {
+				var key = $(this).attr("href");
+				// TODO : Check if this is worthy of counting or not
+				if(tempMap[key] === undefined) {
+					tempMap[key] = 1;
+				}
+			});
+			map[u] = tempMap;
+			csv+=(Object.keys(tempMap).toString());
+			csv+='\n';
+			tempMap = {};
 			console.log(`${ind} : Request Done. Doing something with body`);
 			successfulR++;
 		} else {
-			console.log(`${ind} Request failed`);
+			console.log(`${ind} : Request failed`);
 			failedR++;
 		}
 		// Make next call
@@ -102,6 +119,18 @@ function launchRequest(u, ind) {
 		if(next == trueCount && successfulR+failedR == trueCount) {
 			console.log('All done!');
 			console.log(`${successfulR} were successful and ${failedR} failed.`);
+			console.log('Writing to file ...');
+			writeJSON(csv);
+		}
+	})
+}
+
+function writeJSON(m) {
+	fs.writeFile('map.csv', m, function(err) {
+		if(err) {
+			throw err;
+		} else {
+			console.log('Done!')
 		}
 	})
 }
